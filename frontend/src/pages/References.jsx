525897f561a85aac4ddac39f275dcce388511372
an3177 <137133692+an3177@users.noreply.github.com>
2026-08-18 15:14:34 -0700

    added references code

--- frontend/src/pages/References.jsx
+++ frontend/src/pages/References.jsx
@@ -1,15 +1,4 @@
 function References() {
-    return (
-        <section>
-            <h1>References</h1>
-
-            <ul>
-                <li>CAP Plan</li>
-                <li>GHG Emissions</li>
-                <li>ClimatePlans</li>
-            </ul>
-        </section>
-    )
 }
 
 export default References;
